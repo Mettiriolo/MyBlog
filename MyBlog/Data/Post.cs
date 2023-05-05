8e8e8523d33d7a26588e48af29d0e41cbf22b69c
fan yan <1844609673@qq.com>
2023-05-05 15:21:10 +0800

    添加项目文件。

---
+++ MyBlog/Data/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBlog.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; } = "Mettiriolo";
+        public string Content { get; set; }
+        public string Excerpt { get; set; }
+
+    }
+}
